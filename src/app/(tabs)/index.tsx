2539c23dd2595cedf565e9fb0192c05bd5c0fdd2
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2024-04-02 15:19:43 +0200

    Update TabOneScreen layout and styles
    Refactor TabOneScreen to display product image, name, and price in a more visually appealing layout.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,35 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/src/components/EditScreenInfo";
-import { Text, View } from "@/src/components/Themed";
+import Colors from "@/src/constants/Colors";
+import { StyleSheet, Text, View, Image } from "react-native";
+import products from "@/assets/data/products";
 
 export default function TabOneScreen() {
+  const { name, price, image } = products[0];
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{ uri: image }} style={styles.image} />
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.price}>${price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
   },
   title: {
-    fontSize: 20,
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
     fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  image: {
+    width: "100%",
+    aspectRatio: 1,
   },
 });
