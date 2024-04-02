33cdb656fc3af69190c2afb9eeda9dbc0458be2f
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2024-04-02 15:38:20 +0200

    Add Product type and update ProductListItem component
    Added the Product type definition and updated the ProductListItem component
    to use the new type for product props. Also added a default pizza image
    URL for cases where the product image is null.

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,12 +1,23 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import Colors from "../constants/Colors";
+import { Product } from "../types";
 
-const ProductListItem = (props) => {
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = (props: ProductListItemProps) => {
   const { product } = props;
   const { name, price, image } = product;
   return (
     <View style={styles.container}>
-      <Image source={{ uri: image }} style={styles.image} />
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        style={styles.image}
+      />
       <Text style={styles.title}>{name}</Text>
       <Text style={styles.price}>${price}</Text>
     </View>
