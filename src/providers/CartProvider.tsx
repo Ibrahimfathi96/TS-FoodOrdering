6510f1c0a645466cbd0eb877e05b59b998fb6b79
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2024-04-04 16:39:56 +0200

    Add expo-crypto dependency and implement quantity update in CartProvider
    - Added expo-crypto dependency version 12.8.1
    - Implemented quantity update functionality in CartProvider for cart items.

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,34 +1,59 @@
 import { CartItem, Product } from "@/types";
 import { PropsWithChildren, createContext, useContext, useState } from "react";
-
+import { randomUUID } from "expo-crypto";
 type CartType = {
   items: CartItem[];
   onAddItem: (product: Product, size: CartItem["size"]) => void;
+  updateQuantity: (itemId: string, amount: -1 | 1) => void;
 };
 export const CartContext = createContext<CartType>({
   items: [],
   onAddItem: () => {},
+  updateQuantity: () => {},
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
   const [items, setItems] = useState<CartItem[]>([]);
   const onAddItem = (product: Product, size: CartItem["size"]) => {
     // if the item is already in the cart, increase the quantity
+    const existingItem = items.find(
+      (item) => item.product === product && item.size === size
+    );
+    if (existingItem) {
+      updateQuantity(existingItem.id, 1);
+      return;
+    }
+
     const newCartItem: CartItem = {
       product: product,
       size: size,
       quantity: 1,
       product_id: product.id,
-      id: "1", // TODO: generate unique id
+      id: randomUUID(),
     };
 
     setItems([newCartItem, ...items]);
   };
+
+  // update the quantity of an item in the cart
+  const updateQuantity = (itemId: string, amount: -1 | 1) => {
+    setItems(
+      items
+        .map((item) =>
+          item.id !== itemId
+            ? item
+            : { ...item, quantity: item.quantity + amount }
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
   return (
     <CartContext.Provider
       value={{
         items,
         onAddItem,
+        updateQuantity,
       }}
     >
       {children}
